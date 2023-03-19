62fa7ce6168c03ef8aad80766e91412f6bd7c21e
Liangliang Zhu <shinyypig@gmail.com>
2023-03-19 17:36:45 +0800

    🔥 remove all compress related codes

--- src/config.js
+++ src/config.js
@@ -5,84 +5,71 @@ let fileDirConfig;
 let baseDirConfig;
 let extensionPath;
 let fileExt;
-let compressEnable;
-let compressThreshold;
 ///////////////////////////////////////
 
 function init(context) {
-  fileDirConfig = vscode.workspace.getConfiguration("mdPasteEnhanced")["path"];
-  baseDirConfig =
-    vscode.workspace.getConfiguration("mdPasteEnhanced")["basePath"];
-  extensionPath = context.extensionPath;
-  fileExt = vscode.workspace.getConfiguration("mdPasteEnhanced")["ImageType"];
-  switch (fileExt) {
-    case ".png":
-      break;
-    case ".jpg":
-      break;
-    default:
-      vscode.window.showErrorMessage(`Invalid file extension: ${fileExt}`);
-      throw new Error(`Invalid file extension: ${fileExt}`);
-  }
-  compressEnable =
-    vscode.workspace.getConfiguration("mdPasteEnhanced")["compressEnable"];
-  compressThreshold =
-    vscode.workspace.getConfiguration("mdPasteEnhanced")["compressThreshold"] *
-    1024;
+    fileDirConfig = vscode.workspace.getConfiguration("mdPasteEnhanced")["path"];
+    baseDirConfig =
+        vscode.workspace.getConfiguration("mdPasteEnhanced")["basePath"];
+    extensionPath = context.extensionPath;
+    fileExt = vscode.workspace.getConfiguration("mdPasteEnhanced")["ImageType"];
+    switch (fileExt) {
+        case ".png":
+            break;
+        case ".jpg":
+            break;
+        default:
+            vscode.window.showErrorMessage(`Invalid file extension: ${fileExt}`);
+            throw new Error(`Invalid file extension: ${fileExt}`);
+    }
 }
 
 function calcPathVariables(patternString) {
-  const variablePatterns = [
-    {
-      pattern: /\$\{currentFileDir\}/g,
-      get value() {
-        const editor = vscode.window.activeTextEditor;
-        const filePath = editor.document.uri.fsPath;
-        return path.dirname(filePath);
-      },
-    },
-    {
-      pattern: /\$\{projectRoot\}/g,
-      get value() {
-        return vscode.workspace.workspaceFolders[0].uri.fsPath;
-      },
-    },
-  ];
+    const variablePatterns = [
+        {
+            pattern: /\$\{currentFileDir\}/g,
+            get value() {
+                const editor = vscode.window.activeTextEditor;
+                const filePath = editor.document.uri.fsPath;
+                return path.dirname(filePath);
+            },
+        },
+        {
+            pattern: /\$\{projectRoot\}/g,
+            get value() {
+                return vscode.workspace.workspaceFolders[0].uri.fsPath;
+            },
+        },
+    ];
 
-  for (const pattern of variablePatterns) {
-    patternString = patternString.replace(pattern.pattern, pattern.value);
-  }
-  return path.normalize(patternString);
+    for (const pattern of variablePatterns) {
+        patternString = patternString.replace(pattern.pattern, pattern.value);
+    }
+    return path.normalize(patternString);
 }
 
 module.exports = {
-  init,
-  get fileDir() {
-    return calcPathVariables(fileDirConfig);
-  },
-  get baseDir() {
-    return calcPathVariables(baseDirConfig);
-  },
-  get extensionPath() {
-    return extensionPath;
-  },
-  get fileExt() {
-    return fileExt;
-  },
-  get compressEnable() {
-    return compressEnable;
-  },
-  get compressThreshold() {
-    return compressThreshold;
-  },
-  get renderPattern() {
-    return vscode.workspace.getConfiguration("mdPasteEnhanced")[
-      "renderPattern"
-    ];
-  },
-  get confirmPattern() {
-    return vscode.workspace.getConfiguration("mdPasteEnhanced")[
-      "confirmPattern"
-    ];
-  },
+    init,
+    get fileDir() {
+        return calcPathVariables(fileDirConfig);
+    },
+    get baseDir() {
+        return calcPathVariables(baseDirConfig);
+    },
+    get extensionPath() {
+        return extensionPath;
+    },
+    get fileExt() {
+        return fileExt;
+    },
+    get renderPattern() {
+        return vscode.workspace.getConfiguration("mdPasteEnhanced")[
+            "renderPattern"
+        ];
+    },
+    get confirmPattern() {
+        return vscode.workspace.getConfiguration("mdPasteEnhanced")[
+            "confirmPattern"
+        ];
+    },
 };
--- src/main.js
+++ src/main.js
@@ -3,59 +3,35 @@ const config = require("./config.js");
 const clipboard = require("./clipboard");
 const getFullPath = require("./getFullPath.js");
 const render = require("./render.js");
-const compress = require("./compress.js");
-const logger = require("./logger.js");
-const path = require("path");
 
 async function main(context) {
-  config.init(context);
-  const clipContent = await vscode.env.clipboard.readText(); // 提升速度, 有文字则不用判断是否为图片
-  if (clipContent == "" && (await clipboard.isImage())) {
-    try {
-      pasteImage();
-    } catch (e) {
-      vscode.window.showInformationMessage(e);
-    }
-  } else {
-    if (vscode.window.activeTextEditor.selection.isEmpty == false) {
-      try {
-        vscode.commands.executeCommand("markdown.extension.editing.paste"); // 兼容 markdown all in one
-      } catch (e) {
-        vscode.commands.executeCommand("editor.action.clipboardPasteAction");
-      }
+    config.init(context);
+    const clipContent = await vscode.env.clipboard.readText(); // 提升速度, 有文字则不用判断是否为图片
+    if (clipContent == "" && (await clipboard.isImage())) {
+        try {
+            pasteImage();
+        } catch (e) {
+            vscode.window.showInformationMessage(e);
+        }
     } else {
-      vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+        if (vscode.window.activeTextEditor.selection.isEmpty == false) {
+            try {
+                vscode.commands.executeCommand("markdown.extension.editing.paste"); // 兼容 markdown all in one
+            } catch (e) {
+                vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+            }
+        } else {
+            vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+        }
     }
-  }
 }
 
 async function pasteImage() {
-  const filePath = await getFullPath(config.confirmPattern);
-  render(config.baseDir, filePath); // 先渲染出来
-  const savePath = await clipboard.saveImage(filePath);
-  if (config.compressEnable) {
-    const fileSize = compress.getFilesizeInBytes(savePath);
-    if (fileSize > config.compressThreshold) {
-      compressFile(path.dirname(savePath), savePath);
-    }
-  }
-}
-
-async function compressFile(destDir, sourceFile) {
-  let fileSize = compress.getFilesizeInBytes(sourceFile);
-  logger.log(sourceFile);
-  logger.log(`before fileSize: ${fileSize}`);
-  try {
-    await compress.compressIMG(destDir, [sourceFile]);
-  } catch (e) {
-    logger.log(e);
-  }
-  let fileName = path.basename(sourceFile);
-  let destFile = path.join(destDir, fileName);
-  fileSize = compress.getFilesizeInBytes(destFile);
-  logger.log(`after fileSize: ${fileSize}`);
+    const filePath = await getFullPath(config.confirmPattern);
+    render(config.baseDir, filePath); // 先渲染出来
+    await clipboard.saveImage(filePath);
 }
 
 module.exports = {
-  main,
+    main,
 };
